4281ca94dacfc3e205e7d6b06d79c550b6608b6a
thalamatichandanreddy2007-beep <thalamatichandanreddy.2007@gmail.com>
2026-02-10 09:27:50 +0530

    push second file

---
+++ second.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int p,r,t,si;
+    cout<< "enter the number p: "<<endl;
+    cin>> p;
+    cout<< "enter the number r: "<<endl;
+    cin>> r;
+    cout<< "enter the number t: "<<endl;
+    cin>> t;
+    
+    si= p*r*t;
+    cout<<si<<endl;
+    
+    return 0;
+
+}
